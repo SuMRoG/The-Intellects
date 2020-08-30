4e0d34aee1381ee90618649cb70f3ce809e1d747
blueedgetechno <blueedgetechno@gmail.com>
2020-08-30 12:42:04 +0530

    stash

---
+++ public/js/connect.js
@@ -0,0 +1,9 @@
+function myfunction(){
+  var x= document.getElementsByClassName('wesr');
+  if(x.style.display==="none")
+  {
+    x.style.display="block";
+  } else{
+    x.style.display="none";
+  }
+}
