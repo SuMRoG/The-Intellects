bb95b473dceb2b4353c1b8fb9af77909c2c4fca8
blueedgetechno <blueedgetechno@gmail.com>
2020-09-04 19:42:46 +0530

    heroku

--- public/js/addblog.js
+++ public/js/addblog.js
@@ -33,6 +33,10 @@ function updatepreview(){
     target.innerHTML = html;
 }
 
+async function modify(imgurl) {
+  return imgurl.split("").reverse().join("").replace("051","0821").split("").reverse().join("")
+}
+
 function setbannerimage(ele){
   var url = ele.value.trim()
   if(!url.includes("http")){
@@ -43,12 +47,15 @@ function setbannerimage(ele){
       if(temp[i].length) url.push(temp[i])
     }
 
-    url = "https://pixabay.com/api/?key=16584935-732689d72ee72861d5f1ced4c&per_page=10&image_type=photo&min_width=1000&q="+url.join("+")
-    fetch(url).then(res=> res.json()).then(res=>{
+    url = "https://pixabay.com/api/?key=16584935-732689d72ee72861d5f1ced4c&per_page=20&image_type=photo&min_width=1000&q="+url.join("+")
+    fetch(url).then(res=> res.json()).then(async function(res){
       var imgprev = document.getElementById("blogimagepreview")
       var i = Math.floor(Math.random()*res.hits.length)
-      imgprev.src = res.hits[i].webformatURL;
-      document.getElementById('inputbanner').value = res.hits[i].webformatURL;
+      var imgurl = res.hits[i].previewURL;
+      imgurl = await modify(imgurl)
+      console.log(imgurl);
+      imgprev.src = imgurl;
+      document.getElementById('inputbanner').value = imgurl;
     }).catch(err=> console.log(err))
   }else{
     var imgprev = document.getElementById("blogimagepreview")
